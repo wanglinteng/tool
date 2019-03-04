4d50a2ff2e22c5440bf9428fee20018024de5ec0
lnu server <924214972@qq.com>
2019-03-04 13:48:58 +0800

    update

--- RotatePart/rotatepart.py
+++ RotatePart/rotatepart.py
@@ -1,42 +1,68 @@
 import cv2
+import numpy as np
 
 
-def rotate_part(data, center, theta):
+def rotate_part(part, center, theta):
 
-    left = data.get('left')
-    top = data.get('top')
-    width = data.get('width')
-    height = data.get('height')
-    img = data.get('img')
-    i_w, i_h, _ = img.shape
+    left = part.get('left')
+    top = part.get('top')
+    width = part.get('width')
+    height = part.get('height')
+    data = part.get('data')
 
-    # extract part
-    part = img[top:top+height, left:left+width]
-    cv2.imwrite("./part.png", part)
+    # 计算四个顶点坐标
+    R1 = [left, top]
+    R2 = [left + width, top]
+    R3 = [left, top + height]
+    R4 = [left + width, top + height]
+    R = [R1, R2, R3, R4]
 
-    # calculate rotate location(r_x, r_y)
+    # 计算旋转后顶点坐标
     M = cv2.getRotationMatrix2D(center, theta, 1.0)
-    for x in range(top, top+height):
-        for y in range(left, left+width):
+    R_rotate = []
+    for x, y in R:
+        r_x = M[0][0] * x + M[0][1] * y + M[0][2]
+        r_y = M[1][0] * x + M[1][1] * y + M[1][2]
+        r_x = int(round(r_x))
+        r_y = int(round(r_y))
+        R_rotate.append([r_x, r_y])
+    R_rotate = np.array(R_rotate)
+    R_rotate = np.transpose(R_rotate)
+
+    X_max = max(R_rotate[0])
+    X_min = min(R_rotate[0])
+    Y_max = max(R_rotate[1])
+    Y_min = min(R_rotate[1])
+    print(X_min, Y_min, X_max, Y_max)
+
+    # 计算data部分位置转换
+    dict = {}
+    for x in range(left, left+width):
+        for y in range(top, top+height):
             r_x = M[0][0]*x+M[0][1]*y+M[0][2]
             r_y = M[1][0]*x+M[1][1]*y+M[1][2]
             r_x = int(round(r_x))
             r_y = int(round(r_y))
-            # deal out of bounds
-            if r_x >= i_w or r_y >= i_h:
-                continue
-            img[r_x][r_y] = img[x][y]
-    # set rotated part background
-    img[top:top + height, left:left + width] = (255, 255, 255)
-    cv2.imwrite("./rotated.png", img)
+            dict["%d_%d" % (r_x, r_y)] = data[x-left, y-top]
+
+    # 新区域赋值
+    r_data = np.ones([X_max-X_min, Y_max-Y_min, 3])
+    for x in range(X_min, X_max):
+        for y in range(Y_min, Y_max):
+            if "%d_%d" % (x, y) in dict:
+                r_data[x - X_min][y - Y_min] = dict["%d_%d" % (x, y)]
+            else:
+                r_data[x - X_min][y - Y_min] = [255, 255, 255]
+    print(r_data.shape)
+    cv2.imwrite("./r.png", r_data)
 
 
 if __name__ == '__main__':
-    img = cv2.imread('./test.png')
-    # roi region
-    data = {"left": 10, "top": 5, "width": 20, "height": 20, "img": img}
+    data = cv2.imread('./test.png')
+    # part region
+    part = {"left": 10, "top": 10, "width": 48, "height": 48, "data": data}
     # rotate center location
     center = (40, 40)
     # rotate angle
-    theta = 30
-    rotate_part(data=data, center=center, theta=theta)
+    theta = 15
+    rotate_part(part=part, center=center, theta=theta)
